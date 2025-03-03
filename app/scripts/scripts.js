8c50c0fedbad7a15116cff82f3d81aabdef4c0b4
Jake Miller <jakem@mawwservices.com>
2025-03-03 14:26:35 -0500

    add open and close function to menu/instructions

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,5 +1,7 @@
 const btnHamburger = document.querySelector('.js-hamburger');
+const menu = document.querySelector('.js-menu');
 
 btnHamburger.addEventListener('click', function(){
   btnHamburger.classList.toggle('nav-active');
+  menu.classList.toggle('c-menu--open');
 });
