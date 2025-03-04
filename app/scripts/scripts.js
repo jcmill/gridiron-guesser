e5fa52e13a3299aa03c994fb8e4c8a563c061186
Jake Miller <jakem@mawwservices.com>
2025-03-04 09:56:35 -0500

    adjusting and adding refined trigger and animations for navigation, rm tracking for public scripts

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,7 +1,31 @@
+const nav = document.querySelector('nav');
 const btnHamburger = document.querySelector('.js-hamburger');
-const menu = document.querySelector('.js-menu');
+const menu = document.querySelector('.js-menu__content');
+const menuBlur = document.querySelector('.js-menu--blur');
 
-btnHamburger.addEventListener('click', function(){
-  btnHamburger.classList.toggle('nav-active');
-  menu.classList.toggle('c-menu--open');
+document.addEventListener('click', function(el){
+  if (!btnHamburger.contains(el.target) && el.target === menuBlur) {
+    btnHamburger.removeAttribute('active', '')
+    menuDisplay('remove');
+  } if (btnHamburger.contains(el.target) && !btnHamburger.hasAttribute('active')) {
+    btnHamburger.setAttribute('active', '')
+    menuDisplay('add');
+  } else if (btnHamburger.contains(el.target) && btnHamburger.hasAttribute('active')) {
+    btnHamburger.removeAttribute('active', '')
+    menuDisplay('remove');
+  }  
 });
+
+function menuDisplay(action){
+  if (action === 'add'){   
+    btnHamburger.classList.add('nav-active');
+    menu.classList.add('c-menu__content--open');
+    menuBlur.classList.add('c-menu--blur');
+    menuBlur.classList.remove('hide');
+  } else if (action === 'remove') {
+    btnHamburger.classList.remove('nav-active');
+    menu.classList.remove('c-menu__content--open');
+    menuBlur.classList.remove('c-menu--blur');
+    menuBlur.classList.add('hide');
+  }
+};
--- public/scripts/scripts.js
+++
@@ -1,7 +0,0 @@
-const btnHamburger = document.querySelector('.js-hamburger');
-const menu = document.querySelector('.js-menu');
-
-btnHamburger.addEventListener('click', function(){
-  btnHamburger.classList.toggle('nav-active');
-  menu.classList.toggle('c-menu--open');
-});
