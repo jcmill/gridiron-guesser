14a581f98f38bda442d0591dcc9894d4db348c40
Jake Miller <jakem@mawwservices.com>
2025-03-04 10:08:03 -0500

    fix nav/body interaction issue when hitting tab

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -18,14 +18,20 @@ document.addEventListener('click', function(el){
 
 function menuDisplay(action){
   if (action === 'add'){   
-    btnHamburger.classList.add('nav-active');
-    menu.classList.add('c-menu__content--open');
-    menuBlur.classList.add('c-menu--blur');
+    menu.classList.remove('hide');
     menuBlur.classList.remove('hide');
+    setTimeout(() => {
+      btnHamburger.classList.add('nav-active');
+      menu.classList.add('c-menu__content--open');
+      menuBlur.classList.add('c-menu--blur');
+    }, 0);
   } else if (action === 'remove') {
     btnHamburger.classList.remove('nav-active');
     menu.classList.remove('c-menu__content--open');
     menuBlur.classList.remove('c-menu--blur');
-    menuBlur.classList.add('hide');
+    setTimeout(() => {
+      menu.classList.add('hide');
+      menuBlur.classList.add('hide');
+    }, 500);
   }
 };
