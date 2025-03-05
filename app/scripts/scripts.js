40f6f0d58859100625a7ff1737511f5fd814a775
Jake Miller <jakem@mawwservices.com>
2025-03-05 14:29:41 -0500

    search auto complete options compare player options with mystery player, visual indicators, matching class name change, style changes and cleanup

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -42,46 +42,104 @@ function menuDisplay(action){
   }
 };
 
-let availablePlayers = [
-  'Jake Miller',
-  'Trevor VY',
-  'Matthew Quinn',
-  'Johanna Miller',
-  'Zeke Mill',
-  'Noah M',
-  'Brandon Kennedy',
-  'Max Kennedy'
+let players = [
+  {
+    "longName": "Jake Miller",
+    "con": "AE",
+    "team": "ARI",
+    "pos": "LS",
+    "jerseyNum": "59",
+    "weight": "234",
+    "height": "6'4\"",
+    "exp": "2",
+    "school": "Notre Dame"
+  },
+  {
+    "longName": "Johanna Miller",
+    "con": "NW",
+    "team": "KC",
+    "pos": "RB",
+    "jerseyNum": "11",
+    "weight": "120",
+    "height": "5'6\"",
+    "exp": "3",
+    "school": "Fresno State"
+    },
 ];
 
+const mysteryPlayer = {
+  "longName": "Noah Miller",
+  "con": "AFCW",
+  "team": "NC",
+  "pos": "LS",
+  "jerseyNum": "11",
+  "weight": "120",
+  "height": "6'4\"",
+  "exp": "2",
+  "school": "Notre Dame"
+};
+
 inputSearch.onkeyup = function(){
-  let result = [];
+  let nameResult = [];
   let input = inputSearch.value;
   if(input.length){
-    result = availablePlayers.filter((keyword) => {
-      return keyword.toLowerCase().includes(input.toLowerCase());
+    players.forEach((player) => {
+      if (player.longName.toLowerCase().includes(input.toLowerCase())) {
+        nameResult.push(player);
+      }
     });
   } else if (input.length === 0) {
-    clearResults()
+    clearResults();
   }
-  displaySearchResults(result);
+  displaySearchResults(nameResult);
+};
+
+function SearchDisplayTemplate(player, playerCon, playerTeam, playerPos, playerNum, playerWeight, playerHeight, playerExp, playerSchool) {
+  let html = `
+    <div class="o-player">
+    <h4 class="btn" onclick="selectInput(this)">${player.longName}</h4>
+    <ul>
+      <li class="${playerCon}">${player.con}</li>
+      <li class="${playerTeam}">${player.team}</li>
+      <li class="${playerPos}">${player.pos}</li>
+      <li class="${playerNum}">${player.jerseyNum}</li>
+      <li class="${playerWeight}">${player.weight}</li>
+      <li class="${playerHeight}">${player.height}</li>
+      <li class="${playerExp}">${player.exp}</li>
+      <li class="${playerSchool}">${player.school.toLowerCase().split(' ').map(name => name[0]).join('')}</li>
+    </ul>
+  </div>`;
+
+  return html;
 };
 
 function displaySearchResults(result) {
-  const content = result.map((list) => {
-    return `
-    <div class="o-player">
-      <h4 class="btn" onclick="selectInput(this)">${list}</h4>
-    </div>
-    `;
+  const content = result.map(player => {
+    const playerCon = player.con === mysteryPlayer.con ? 'match' : 'no-match';
+    const playerTeam = player.Team === mysteryPlayer.con ? 'match' : 'no-match';
+    const playerPos = player.pos === mysteryPlayer.pos ? 'match' : 'no-match';
+    const playerNum = player.jerseyNum === mysteryPlayer.jerseyNum ? 'match' : 'no-match';
+    const playerWeight = player.weight === mysteryPlayer.weight ? 'match' : 'no-match';
+    const playerHeight = player.height === mysteryPlayer.height ? 'match' : 'no-match';
+    const playerExp = player.exp === mysteryPlayer.exp ? 'match' : 'no-match';
+    const playerSchool = player.school === mysteryPlayer.school ? 'match' : 'no-match';
+
+    return SearchDisplayTemplate(player, playerCon, playerTeam, playerPos, playerNum, playerWeight, playerHeight, playerExp, playerSchool);
   });
   searchResults.innerHTML = content.join('');
 };
 
-function selectInput(list) {
-  inputSearch.value = list.innerHTML;
+function selectInput(playerInfo) {
+  inputSearch.value = playerInfo.innerHTML;
   clearResults()
 }
 
 function clearResults() {
   searchResults.innerHTML = '';
 }
+
+document.addEventListener('click', function(){
+  if(inputSearch === document.activeElement) {
+    console.log('focused');
+  }
+})
